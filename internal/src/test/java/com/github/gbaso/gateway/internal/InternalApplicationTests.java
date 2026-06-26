7f9c0eafa3b88ad0e5f65956b3cbf487a59b191d
Giacomo Baso <gbaso@users.noreply.github.com>
2026-06-26 11:57:27 +0200

    initial commit

---
+++ internal/src/test/java/com/github/gbaso/gateway/internal/InternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.gateway.internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
